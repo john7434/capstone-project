2ae5e8fc527f6bb5406bec8b831e20f1ae4454db
John O'Loughlin <johnoloughlin33@outlook.com>
2020-11-09 19:14:46 -0600

    heroku run build addition

--- components/views/Bio.js
+++ components/views/Bio.js
@@ -4,7 +4,7 @@ export default () => `
 <img class=logoimage src= "https://cdn.discordapp.com/attachments/342162377355034624/774107319600152576/2020-11-05.png">
 <h2 class= title>Welcome to Covid Distanced</h2>
   <img class=coverimage src= "https://mediacloud.kiplinger.com/image/private/s--qtRsm2mb--/t_primary-image-desktop@1/v1584619480/slideshow/investing/T052-S001-10-solid-social-distancing-stocks-to-buy/images/intro.jpg">
-  <p class=description>This website is dedicated torwards ensuring that the public knows the dangers of covid, and the safety neccesary torwards preventing exposure.</p>
+  <p class=description>This website is dedicated torwards ensuring that the public knows the dangers of covid, and the safety necessary torwards preventing exposure.</p>
 
   <p></p>
 <!-- <script src="./index.js"></script> -->
