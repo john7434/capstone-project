6aee57de5b57aea9c52cf1b7522af9980c7bed64
John O'Loughlin <johnoloughlin33@outlook.com>
2020-11-10 16:56:13 -0600

    forms updated

--- components/views/Bio.js
+++ components/views/Bio.js
@@ -1,5 +1,19 @@
 export default () => `
 <section id="bio">
-<body> <!-- <script src="./index.js"></script> -->
+<body>
+
+<p class= "mask">You may think that you already know everything that comes with preparing for Covid-19, but there are a plethora of different attributes you have to worry about. From proper face mask usage(as seen below), to what types of face masks are best for what type of person needs it. From the adult, to the infant. We all must stay safe no matter what.</p>
+
+<center><iframe class="youtube" width="560" height="315" src="https://www.youtube.com/embed/lOLTSRa5CeI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
+<!-- <script src="./index.js"></script> -->
+
+
+
 </body>
-  </section>`;
+</section>`;
+
+
+
+
+
+
