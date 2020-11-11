aeb1927a8b5d71dc7193c7de6b03dee481dc4aee
John O'Loughlin <johnoloughlin33@outlook.com>
2020-11-11 17:33:40 -0600

    updated fonts

--- components/views/Bio.js
+++ components/views/Bio.js
@@ -4,17 +4,25 @@ export default () => `
 <div>
 <p class= "mask">You may think that you already know everything that comes with preparing for Covid-19, but there are a plethora of different attributes you have to worry about. From proper face mask usage(as seen below), to what types of face masks are best for what type of person needs it. From the adult, to the infant. We all must stay safe no matter what.</p>
 </div>
+
 <div>
 <iframe class="youtube" width="560" height="315" src="https://www.youtube.com/embed/lOLTSRa5CeI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 </div>
 
+<div id="biotop">
+<div>
 <img class="adult" src= "https://www.cdc.gov/coronavirus/2019-ncov/images/prevent-getting-sick/do-choose-masks-medium.jpg">
+</div>
 
+<div>
 <img class= "glasses" src= "https://www.cdc.gov/coronavirus/2019-ncov/images/prevent-getting-sick/special-situations-glasses-medium.jpg">
+</div>
 
+<div>
 <img class= "child" src= "https://www.cdc.gov/coronavirus/2019-ncov/images/prevent-getting-sick/special-situations-children-medium.jpg">
+</div>
+</div>
 
-
-<p class= "safety">What you see above is all of the info you need for selection of a mask, pertaining to whether or not the person is an adult, or a child, to an infant. And of course for people with glasses as well.
+<p class= "safety">What you see above is all of the info you need for selection of a mask, pertaining to whether or not the person is an adult, or a child, to an infant. And of course for people with glasses as well. Because while it may seem as though you can just "put a mask on and be safe", the point of it is to filter the air you breathe, whether inhaling or exhaling. So making sure that your mask fits to your face snuggly, but comfortably is a key factor you must take into consideration</p>
 </section>`;
