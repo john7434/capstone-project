99aab834b51e24791785516d74e6640bdade0682
John O'Loughlin <johnoloughlin33@outlook.com>
2020-11-11 00:41:54 -0600

    yeehaw

--- components/views/Home.js
+++ components/views/Home.js
@@ -4,6 +4,6 @@ export default st => `<section>
 <p class=description>This website is dedicated towards ensuring that the public knows the dangers of covid, and the safety necessary towards preventing exposure.</p>
 
 <img class=coverimage src= "https://mediacloud.kiplinger.com/image/private/s--qtRsm2mb--/t_primary-image-desktop@1/v1584619480/slideshow/investing/T052-S001-10-solid-social-distancing-stocks-to-buy/images/intro.jpg">
-  <p class="homestatement">I love you Honey!!!</p>
+  <p class="homestatement">What is COVID-19?</p>
   <p class= "intro">Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus. Most people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special treatment.  Older people, and those with underlying medical problems like cardiovascular disease, diabetes, chronic respiratory disease, and cancer are more likely to develop serious illness. The best way to prevent and slow down transmission is to be well informed about the COVID-19 virus, the disease it causes and how it spreads. Protect yourself and others from infection by washing your hands or using an alcohol based rub frequently and not touching your face. The COVID-19 virus spreads primarily through droplets of saliva or discharge from the nose when an infected person coughs or sneezes, so it’s important that you also practice respiratory etiquette.</p>
 </section>`
