0b7f014a033c9d3fcd775b9e7854fef5bbdcd58c
John O'Loughlin <johnoloughlin33@outlook.com>
2020-11-11 18:44:30 -0600

    updated info and styling

--- components/views/Home.js
+++ components/views/Home.js
@@ -6,10 +6,18 @@ export default st => `<section>
 <img class=coverimage src= "https://mediacloud.kiplinger.com/image/private/s--qtRsm2mb--/t_primary-image-desktop@1/v1584619480/slideshow/investing/T052-S001-10-solid-social-distancing-stocks-to-buy/images/intro.jpg">
 
   <p class="homestatement">What is COVID-19?</p>
-
+<div class="homeparagraphphoto">
+<div>
   <p class= "intro">Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus. Most people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special treatment.  Older people, and those with underlying medical problems like cardiovascular disease, diabetes, chronic respiratory disease, and cancer are more likely to develop serious illness. The best way to prevent and slow down transmission is to be well informed about the COVID-19 virus, the disease it causes and how it spreads. Protect yourself and others from infection by washing your hands or using an alcohol based rub frequently and not touching your face. The COVID-19 virus spreads primarily through droplets of saliva or discharge from the nose when an infected person coughs or sneezes, so it’s important that you also practice respiratory etiquette.</p>
-
+</div>
+<div>
+  <img class="coronaphoto" src="https://media.istockphoto.com/photos/concept-of-sarscov2-or-2019ncov-coronavirus-picture-id1208953647?k=6&m=1208953647&s=612x612&w=0&h=SyIWApq2GQ3W2Be0Ez7gJ0C972kp86XwnkzOMa4dCsY=">
+</div>
+</div>
 <p class="coronalight">Why we cannot take COVID-19 lightly</p>
 
-<p class="coronalightdes">First and foremost, we cannot take COVID-19 lightly because of the effects that it already has over the entire nation of the U.S.
+<p class="coronalightdes">First and foremost, we cannot take COVID-19 lightly because of the effects that it already has over the entire nation of the United States. Across the United States we have had a confirmed 10,000,000+ cases, thats about a fifth of the global cases in total. We also are leading globally in deaths as well, having past 250,000 deaths. But yet across the country men, women, and children are annoyed and bored of having to quarantine. But if the United States had taken a much more serious approach at the beginning of this pandemic, then I wouldn't be making this website. And all of our lives would be back to normal. But unfortunately, our government failed us, and we failed each other, because the mentality that most americans took on at the beginning was. "Other people may catch it, but I'm not other people." And this website hasn't been made to slander and demonize the american people, but to help them realize the mistakes, take responsibility, and move forward from it.</p>
+
+<p>Sample Title</p>
+<p>Sample Description</p>
 </section>`
