afad3707bd6812d4fc1b3dfa6e777e7d682a5107
John O'Loughlin <johnoloughlin33@outlook.com>
2020-11-11 19:41:51 -0600

    pleas work

--- components/views/Form.js
+++ components/views/Form.js
@@ -3,7 +3,7 @@ export default () => `
 <center>
 <form class="form" action="https://formspree.io/f/xeqpzezo" method="POST">
   <input type="text" name="name">
-  <input type="email" name="_replyto">
+  <input type="message" name="put your tip here!">
   <input type="submit" value="Send">
 </form>
 </center>
