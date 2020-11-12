d0f837fbf8d069b45d0682ef71ddd40deb6c400d
John O'Loughlin <johnoloughlin33@outlook.com>
2020-11-11 18:58:54 -0600

    please work form

--- components/views/Form.js
+++ components/views/Form.js
@@ -1,25 +1,22 @@
 export default () => `
 
 <center>
-<form class="form" action="https://formspree.io/f/xqkglebv" method="POST">
-  <p class="form">
-    <label class="form">Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p class="form">
-    <label class="form">Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p class="form">
-    <label class="form">Submission Type: <select name="role[]" multiple>
-      <option class="form">Client</option>
-      <option class="form">Admin</option>
-      <option class="form">Queries</option>
-    </select></label>
-  </p>
-  <p class="form">
-    <label class="form">Message: <textarea name="message"></textarea></label>
-  </p>
-  <p class="form">
-    <button type="submit">Send</button>
-  </p>
+<form class="form"
+  action="https://formspree.io/f/xqkglebv"
+  method="POST"
+>
+  <label>
+    Your email:
+    <input type="text" name="_replyto">
+  </label>
+  <label>
+    Your message:
+    <textarea name="message"></textarea>
+  </label>
+
+  <!-- your other form fields go here -->
+
+  <button type="submit">Send</button>
 </form>
-</center>`;
+</center>
+`;
